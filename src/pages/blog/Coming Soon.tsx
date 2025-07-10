695be4a9c7572c7cc85371bc9a223cf0024b49b4
Kobeli50 <98336013+Kobeli50@users.noreply.github.com>
2025-07-09 23:27:19 -0400

    o

--- src/pages/blog/Coming Soon.tsx
+++ src/pages/blog/Coming Soon.tsx
@@ -8,13 +8,10 @@ const FirstPost: React.FC = () => (
         ← Back to Blogs
       </button>
     </Link>
-    <h1 className="text-3xl font-bold mb-4">My First Blog Post</h1>
+    <h1 className="text-3xl font-bold mb-4">Coming Soon!!!</h1>
     <p className="text-gray-500 text-sm mb-2">2024-05-01</p>
     <div className="prose">
-      <p>
-        Welcome to my first blog post! This is where you can write your content.
-        You can add images, code snippets, and more.
-      </p>
+      <p>Coming Soon!!!</p>
     </div>
   </div>
 );
